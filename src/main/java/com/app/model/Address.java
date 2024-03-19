81f46120d7119336b0f667522e05697938251c70
supriyaghuge123 <supriyaghuge1993@gmail.com>
2024-03-19 15:09:55 +0530

    2nd commit-Address Class

---
+++ src/main/java/com/app/model/Address.java
@@ -0,0 +1,21 @@
+package com.app.model;
+
+public class Address {
+	
+	private String areaname;
+	private String cityname;
+	public String getAreaname() {
+		return areaname;
+	}
+	public void setAreaname(String areaname) {
+		this.areaname = areaname;
+	}
+	public String getCityname() {
+		return cityname;
+	}
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+	
+
+}
